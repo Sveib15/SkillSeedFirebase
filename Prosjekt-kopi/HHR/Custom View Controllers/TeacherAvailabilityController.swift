232238b13cd65a0dcc640675df0e31975a47c2cd
Anders Berntsen <anders@angellions.com>
2018-04-24 14:53:33 +0200

    Added GeoFire support, set up availability for location services.

--- Prosjekt-kopi/HHR/Custom View Controllers/TeacherAvailabilityController.swift
+++ Prosjekt-kopi/HHR/Custom View Controllers/TeacherAvailabilityController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 import Firebase
+import GeoFire
+import CoreLocation
 
-class TeacherAvailabilityController: UIViewController {
+class TeacherAvailabilityController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var skillSelector: CustomSegmentControl!
     
     var ref: DatabaseReference!
+    var GeoRef: GeoFire!
+    var locationManager = CLLocationManager()
+    var selectedSkill = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,13 +27,70 @@ class TeacherAvailabilityController: UIViewController {
         self.navigationItem.setRightBarButton(UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(saveToDatabase)), animated: true)
         
         ref = Database.database().reference()
-    }
+        GeoRef = GeoFire(firebaseRef: ref.child("Locations"))
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+    }// End viewDidLoad
     
+    func setMyLocation(userId: String, dbBranch: GeoFire){
+        
+        var currentLocation: CLLocation!
+        
+        if( CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedWhenInUse ||
+            CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedAlways){
+            
+            currentLocation = locationManager.location
+            print("FØRSTE SHID + \(currentLocation.coordinate.latitude) + HER ER DEN ANDRE SHIDEN + \(currentLocation.coordinate.longitude) ")
+            
+            
+            dbBranch.setLocation(CLLocation(latitude: currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude), forKey: userId) { (error) in
+                if (error != nil) {
+                    print("An error occured: \(error ?? "" as! Error)")
+                } else {
+                    print("Saved location successfully!")
+                }//end if errorhandler
+            }//end GeoRef
+        }//end if
+    }//end setMyLocation
     
     @objc func saveToDatabase() {
         
+        guard let uid = Auth.auth().currentUser?.uid
+            else {
+                return
+        }
+        let beginnerRef = GeoFire(firebaseRef: ref.child("Locations").child("Beginner"))
+        let goodRef = GeoFire(firebaseRef: ref.child("Locations").child("Good"))
+        let adeptRef = GeoFire(firebaseRef: ref.child("Locations").child("Adept"))
+        let eliteRef = GeoFire(firebaseRef: ref.child("Locations").child("Elite"))
         
+        switch selectedSkill {
+        case 0:
+            setMyLocation(userId: uid, dbBranch: beginnerRef)
+        case 1:
+            setMyLocation(userId: uid, dbBranch: goodRef)
+        case 2:
+            setMyLocation(userId: uid, dbBranch: adeptRef)
+        case 3:
+            setMyLocation(userId: uid, dbBranch: eliteRef)
+        default:
+            print("Whoops 1 - default i switchen")
+            return
+        }
+        Shared.shared.tabBarIndex = 3
+        let RegController = self.storyboard?.instantiateViewController(withIdentifier: "tabBarController")
+        self.present(RegController!, animated: true, completion: nil)
     }
+    
+    @IBAction func skillChanged(_ sender: CustomSegmentControl) {
+        selectedSkill = sender.selectedSegmentIndex
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
